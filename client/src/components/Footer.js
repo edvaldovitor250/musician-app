a2f599bc80aef1d0238a8bd5e259c0b719fb0a09
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-07-29 19:04:13 -0300

    organizing project

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="footer">
       <div className="content has-text-centered">
         <p>
-           Musician App 2019. The source code is licensed MIT. The website content is licensed CC BY NC SA 4.0.
+           Musician App 2023. O código-fonte é licenciado pelo MIT. O conteúdo do site é licenciado CC BY NC SA 4.0.
         </p>
       </div>
     </footer>
